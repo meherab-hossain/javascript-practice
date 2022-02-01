e4aed04ba079e666a51c47084b049fe8fb5a39f5
meherab-hossain <h.meherabhossain@gmail.com>
2022-02-01 10:51:36 +0600

    todo version-1 initialized

---
+++ vanila-to-do-v1/index.js
@@ -0,0 +1,49 @@
+let inputBox = document.querySelector('.inputField input')
+let addTodo = document.querySelector('.inputField button')
+let todoList = document.querySelector('.todoList')
+
+inputBox.onkeyup =()=> {
+    let userInput = inputBox.value;
+    if (userInput.trim() != 0) {
+        addTodo.classList.add("active");
+    }else{
+        addTodo.classList.remove("active");
+    }
+}
+let todoItems=[];
+// window.addEventListener('DOMContentLoaded',()=>{
+   
+// })
+showTodoList();
+addTodo.addEventListener('click',()=> {  
+    let userInput = inputBox.value;
+    
+    todoItems.push(userInput)
+
+    localStorage.setItem("Todo", JSON.stringify(todoItems));
+    inputBox.value='';
+    showTodoList();
+});
+
+
+function showTodoList() {    
+    let getLocalStorageData = localStorage.getItem("Todo");
+    
+    todoItems = JSON.parse(getLocalStorageData);
+    //console.log(todoItems);
+    todoList.innerHTML='';
+    if (todoItems && todoItems.length) {
+        todoItems.forEach((element, index)=>{
+            todoList.innerHTML+=`<li>${element}<span class="icon" onclick="deleteTodos(${index})"><i class="fas fa-trash"></i></span></li>`
+        })
+    }
+   
+}
+function deleteTodos(index) {
+    let getLocalStorageData = localStorage.getItem("Todo");
+    
+    todoItems = JSON.parse(getLocalStorageData);
+    todoItems.splice(index,1);
+    localStorage.setItem("Todo", JSON.stringify(todoItems));
+    showTodoList();
+}
