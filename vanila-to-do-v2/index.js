9f1ddb97a8ee647fbb69225681739eb659a791d1
meherab-hossain <h.meherabhossain@gmail.com>
2022-02-02 12:49:48 +0600

    added checkbox and clear all functionality

--- vanila-to-do-v2/index.js
+++ vanila-to-do-v2/index.js
@@ -1,8 +1,10 @@
+//Selectors
 let inputBox = document.querySelector('.inputField input')
 let addTodo = document.querySelector('.inputField button')
 let todoList = document.querySelector('.todoList')
 let clearAllTodos = document.querySelector('.footer button')
 
+//helpers
 inputBox.onkeyup =()=> {
     let userInput = inputBox.value;
     if (userInput.trim() != 0) {
@@ -27,7 +29,7 @@ addTodo.addEventListener('click',()=> {
     
     const todos={
         id:Date.now(),
-        checked:false,
+        completed:false,
         item: userInput
 
     }
@@ -35,7 +37,7 @@ addTodo.addEventListener('click',()=> {
        
         todoItems.push(todos)
         localStorage.setItem("Todo", JSON.stringify(todoItems));
-        clearAllTodos.classList.add("active");
+        
         addTodo.classList.remove("active");
         inputBox.value='';
     }
@@ -44,7 +46,13 @@ addTodo.addEventListener('click',()=> {
     showTodoList();
     
 });
+clearAllTodos.addEventListener('click',()=>{
+    todoItems=[];
+    localStorage.setItem("Todo",JSON.stringify(todoItems))
+    showTodoList();
+})
 
+//function
 
 function showTodoList() {    
     let getLocalStorageData = localStorage.getItem("Todo");
@@ -53,11 +61,13 @@ function showTodoList() {
    
     todoList.innerHTML='';
     if (todoItems && todoItems.length) {
+        clearAllTodos.classList.add("active");
         todoItems.forEach((element, index)=>{
-            todoList.innerHTML+=`<li>${element.item}<span class="icon" onclick="deleteTodos(${element.id})"><i class="fas fa-trash"></i></span></li>`
+            todoList.innerHTML+=`<li><input class="checkbox__margin" onchange="checkTodoItems(${element.id})" type="checkbox" id="${element.id}"><span>${element.item}</span><span class="icon" onclick="deleteTodos(${element.id})"><i class="fas fa-trash"></i></span></li>`
         })
     }else{
         todoItems=[];
+        clearAllTodos.classList.remove("active");
     }
    
 }
@@ -79,9 +89,30 @@ function deleteTodos(id) {
     showTodoList();
 }
 
-clearAllTodos.addEventListener('click',()=>{
-    todoItems=[];
-    localStorage.setItem("Todo",JSON.stringify(todoItems))
-    clearAllTodos.classList.remove("active");
-    showTodoList();
-})
+
+function checkTodoItems(todoId) {
+    let getLocalStorageData = localStorage.getItem("Todo");
+    let temp= document.getElementById(todoId);
+    let nextSibling = temp.nextElementSibling;
+    todoItems = JSON.parse(getLocalStorageData);
+    if (temp.checked) {
+        todoItems.filter((el)=>{
+            if (el.id === todoId) {
+                el.completed=true;
+                console.log(el);
+                //localStorage.setItem("Todo",JSON.stringify(el))
+            }
+        })        
+        nextSibling.style.textDecoration="line-through"
+    }else{
+        todoItems.filter((el)=>{
+            if (el.id === todoId) {
+                el.completed=false;
+                console.log(el);
+                //localStorage.setItem("Todo",JSON.stringify(el))
+            }
+        }) 
+        nextSibling.style.textDecoration="none"
+    }
+    
+}
